aad3f5fe8fa6dd8815a2b904ee3a79b4bd2c70a7
ArminSebastian <arminsebastian@gmail.com>
2015-10-30 17:01:12 -0700

    solve boleans and numbers value at bit

--- app/numbers.js
+++ app/numbers.js
@@ -2,7 +2,8 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    var binary = num.toString(2);
+    return parseInt( binary[binary.length - bit], 10 );
   },
 
   base10: function(str) {
