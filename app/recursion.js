4b1bb5b3d12bfe09f914e474b454b06bb852fb62
ArminSebastian <arminsebastian@gmail.com>
2015-11-02 16:45:19 -0800

    solve listFiles recursively

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,19 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+    var list = [];
+    var dir = data.files;
+    function subrutine (d) {
+      for (var i = 0; i < d.length; i++) {
+        if ( (typeof d[i]) === 'string') {
+          list.push(d[i]);
+        } else if ( typeof d[i] === 'object'){
+          subrutine(d[i].files);
+        }
+      }
+    }
+    subrutine(dir);
+    return list;
   },
 
   permute: function(arr) {
