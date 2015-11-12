2ff3414c6d6afc6464da40e6cbd2b07e4ce8f4ca
ArminSebastian <arminsebastian@gmail.com>
2015-11-12 14:25:31 -0800

    solve Async .then async

--- app/async.js
+++ app/async.js
@@ -2,7 +2,13 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var myObj = {
+      args: value,
+      then: function (callback) {
+        return setTimeout(callback, 0, myObj.args);
+      }
+    };
+    return myObj;
   },
 
   manipulateRemoteData : function(url) {
